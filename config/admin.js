838dd0a17ebd74532988b27641b656da476e6b40
David Blatt <davidblatt10@gmail.com>
2022-04-19 09:27:09 -0400

    changed admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  apiToken: {
-    salt: env("API_TOKEN_SALT", "d9b0df66ff97a666027e665707b4e3e7"),
-  },
   auth: {
-    secret: env("ADMIN_JWT_SECRET", "48556701af0686667faabccb303619ae"),
+    secret: env("ADMIN_JWT_SECRET"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
   },
 });
